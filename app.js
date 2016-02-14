da7ad8be236d9e3e54518a5bc09c2dbdbcf3b13a
Georgios Lymperopoulos <glympe77@gmail.com>
2016-02-14 14:54:24 +0000

    Added bootstrap - jquery. Created a form to add a new beach. Some basic styling added.

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require("express");
 var bodyParser = require("body-parser")
+
 var app = express();
+
+//Serve contents of public directory
+app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+
+
+var beaches = [
+            {
+                name: "Menidi", image: "https://sailchecker.com/wp-content/uploads/2015/06/best-greek-beaches-seychelles-beach-ikaria.jpg"
+            },
+            {
+                name: "Koronisia", image: "http://www.beacheszone.com/wp-content/uploads/2012/10/pori_beach_isyub.jpg"
+            },
+            {
+                name: "Kastrosykia", image: "http://www.honeysucklelife.com/wp-content/uploads/2012/05/Greek-Beaches-Zakynthos.jpg"
+            }
+        ];
 
 
 app.get("/", function(req, res){
@@ -10,19 +28,19 @@ app.get("/", function(req, res){
 });
 
 app.get("/beaches", function(req, res) {
-    var beaches = [
-            {
-                name: "Menidi", image: "https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e15/11410430_1643890539185282_1517816764_n.jpg"
-            },
-            {
-                name: "Koronisia", image: "http://1.bp.blogspot.com/-3m94WEX25Bs/Ug8nms2QKGI/AAAAAAAAiNA/pSe-nD6krB0/s1600/%25CE%25BA%25CE%25BF%25CF%2581%25CF%2589%25CE%25BD%25CE%25B7%25CF%2583%25CE%25AF%25CE%25B1.jpg"
-            },
-            {
-                name: "Kastrosykia", image: "https://media-cdn.tripadvisor.com/media/photo-s/02/bd/6c/b8/preveza-beach-hotel.jpg"
-            }
-        ];
-        
-    res.render("beaches", {beaches:beaches})
+    res.render("beaches", {beaches:beaches});
+});
+
+app.post("/beaches", function(req, res){
+   var beachName = req.body.name;
+   var beachImg = req.body.image;
+   
+   beaches.push({name: beachName, image: beachImg});
+   res.redirect("/beaches");
+});
+
+app.get("/beaches/new", function(req, res){
+    res.render("new.ejs");
 });
 
 
