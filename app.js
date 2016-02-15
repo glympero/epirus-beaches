c799b3fd409f65880397466f38e63096b4b61ee5
Georgios Lymperopoulos <glympe77@gmail.com>
2016-02-15 16:16:32 +0000

    App is now connected to mongo db. Updated routes. Added a show page without styling yet.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser")
+var mongoose = require("mongoose");
 
 var app = express();
+var dbURI = 'mongodb://localhost/beaches_epirus';
+mongoose.connect(dbURI);
+//mongoose.connect("mongodb://localhost/beaches_epirus");
 
 //Serve contents of public directory
 app.use(express.static("public"));
@@ -9,38 +13,78 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
+//DB Schema
+var beachesSchema = new mongoose.Schema({
+   name: String, 
+   image: String,
+   description: String
+});
 
-var beaches = [
-            {
-                name: "Menidi", image: "https://sailchecker.com/wp-content/uploads/2015/06/best-greek-beaches-seychelles-beach-ikaria.jpg"
-            },
-            {
-                name: "Koronisia", image: "http://www.beacheszone.com/wp-content/uploads/2012/10/pori_beach_isyub.jpg"
-            },
-            {
-                name: "Kastrosykia", image: "http://www.honeysucklelife.com/wp-content/uploads/2012/05/Greek-Beaches-Zakynthos.jpg"
-            }
-        ];
+var Beaches = mongoose.model('Beaches', beachesSchema);
+
+// Beaches.create({
+//                 name: "Sivota", 
+//                 image: "http://img6.charmio.com/images/2495-ornella-beach-resort-villas-sivota-thesprotia-greece-03-600.jpg",
+//                 description: "This is a great beach with crystal clear waters."
+//             },function(err, beach){
+//                 if(err){
+//                     console.log(err)
+//                 } else {
+//                     console.log(beach);
+//             } 
+//     });
 
 
 app.get("/", function(req, res){
     res.render("home");
 });
 
+//INDEX ROUTE - show all beaches
 app.get("/beaches", function(req, res) {
-    res.render("beaches", {beaches:beaches});
+    Beaches.find({}, function(err, beaches){
+        if(err){
+            console.log(err);
+        }else {
+            res.render("index", {beaches:beaches});
+        }
+    });
 });
 
+//CREATE ROUTE - add new beach to DB
 app.post("/beaches", function(req, res){
    var beachName = req.body.name;
    var beachImg = req.body.image;
-   
-   beaches.push({name: beachName, image: beachImg});
-   res.redirect("/beaches");
+   var beachDesc = req.body.desc;
+    Beaches.create({
+                name: beachName, 
+                image: beachImg,
+                description: beachDesc
+            },function(err, beach){
+                if(err){
+                    console.log(err)
+                } else {
+                    res.redirect("/beaches");
+            } 
+    });
 });
 
+//NEW ROUTE - show form to create new beach
 app.get("/beaches/new", function(req, res){
     res.render("new.ejs");
+});
+
+//SHOW ROUTE - show details of a selected beach
+app.get("/beaches/:id", function(req, res){
+    
+    var beachId = req.params.id;
+    
+    Beaches.findById(beachId, function(err, beach){
+        if(err) {
+            console.log(err);
+        }else{
+            res.render("show", {beach: beach});
+        }
+    });
 });
 
 
@@ -52,4 +96,15 @@ app.get("/beaches/new", function(req, res){
 app.listen(process.env.PORT, process.env.IP, function(){
 //app.listen(8080, "127.0.0.1", function(){
    console.log("Server Running"); 
+});
+
+//Monitoring moongose
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
 });
